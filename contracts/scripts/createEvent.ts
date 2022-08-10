f960570225f45f33cf34a230052812b8f34cba47
ben <phas0ruk@gmail.com>
2022-08-09 20:34:23 -0700

    integrating smart contracts on create event form

--- contracts/scripts/createEvent.ts
+++ contracts/scripts/createEvent.ts
@@ -3,49 +3,51 @@ import * as EventContractFactoryJson from "../artifacts/contracts/EventContractF
 import { EventContractFactory } from "../typechain-types";
 import { getWallet, getRopstenProvider } from "../utils";
 import * as EventABI from "../artifacts/contracts/EventContractFactory.sol/EventContractFactory.json";
+import { eventFactoryAddress } from "../eventFactoryContractAddress";
 
-const eventFactoryAddress = "0xf99F908CbE90B1a6eb86Df5F561F20910c3A9a38";
+//const eventFactoryAddress = "0xbd4498dA203c726858fC07687E4f17AF29cA071f";
 const eventName = "GopherCon";
 const location = "London";
 const date = new Date(2023, 1, 1, 0, 0, 0, 0);
+console.log(`Date: ${date}`);
 const numberOfTickets = 1000;
 const ticketPrice = ethers.utils.parseEther("0.5");
 
 async function main() {
-  const wallet = getWallet();
-  const provider = getRopstenProvider();
-  const signer = wallet.connect(provider);
+    const wallet = getWallet();
+    const provider = getRopstenProvider();
+    const signer = wallet.connect(provider);
 
-  const eventCreatorContract = new ethers.Contract(
-    eventFactoryAddress,
-    EventContractFactoryJson.abi,
-    signer
-  ) as EventContractFactory;
+    const eventCreatorContract = new ethers.Contract(
+        eventFactoryAddress,
+        EventContractFactoryJson.abi,
+        signer
+    ) as EventContractFactory;
 
-  console.log("setting up listeners...");
-  const eventCreateFilter = eventCreatorContract.filters.EventCreation();
-  provider.once(eventCreateFilter, ({ topics, data }) => {
-    const iface = new ethers.utils.Interface(EventABI.abi);
-    const parsedLog = iface.parseLog({ topics, data });
-    console.log("PARSED LOG:", parsedLog);
-  });
+    console.log("setting up listeners...");
+    const eventCreateFilter = eventCreatorContract.filters.EventCreation();
+    provider.once(eventCreateFilter, ({ topics, data }) => {
+        const iface = new ethers.utils.Interface(EventABI.abi);
+        const parsedLog = iface.parseLog({ topics, data });
+        console.log("PARSED LOG:", parsedLog);
+    });
 
-  console.log("creating new event...");
-  const tx = await eventCreatorContract.createEvent(
-    eventName,
-    location,
-    date.getTime(),
-    numberOfTickets,
-    ticketPrice
-  );
-  await tx.wait();
+    console.log("creating new event...");
+    const tx = await eventCreatorContract.createEvent(
+        eventName,
+        location,
+        date.getTime(),
+        numberOfTickets,
+        ticketPrice
+    );
+    await tx.wait();
 
-  console.log(`event created with transaction hash ${tx.hash}`);
+    console.log(`event created with transaction hash ${tx.hash} `);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+    console.error(error);
+    process.exitCode = 1;
 });
--- contracts/scripts/deployEventContractFactory.ts
+++ contracts/scripts/deployEventContractFactory.ts
@@ -2,30 +2,64 @@ import { ethers } from "ethers";
 import * as EventContractFactoryJson from "../artifacts/contracts/EventContractFactory.sol/EventContractFactory.json";
 import { EventContractFactory } from "../typechain-types";
 import { getWallet, getRopstenProvider } from "../utils";
+import fs from "fs";
 
 const fee = ethers.utils.parseEther("0.1");
 
 async function main() {
-  const wallet = getWallet();
-  const provider = getRopstenProvider();
-  const signer = wallet.connect(provider);
+    const wallet = getWallet();
+    const provider = getRopstenProvider();
+    const signer = wallet.connect(provider);
 
-  const contractFactory = new ethers.ContractFactory(
-    EventContractFactoryJson.abi,
-    EventContractFactoryJson.bytecode,
-    signer
-  );
-  const eventCreator = (await contractFactory.deploy(
-    fee
-  )) as EventContractFactory;
-  await eventCreator.deployed();
+    const contractFactory = new ethers.ContractFactory(
+        EventContractFactoryJson.abi,
+        EventContractFactoryJson.bytecode,
+        signer
+    );
+    const eventCreator = (await contractFactory.deploy(
+        fee
+    )) as EventContractFactory;
+    await eventCreator.deployed();
 
-  console.log(`Contract deployed with address: ${eventCreator.address}`);
+    console.log(`Contract deployed with address: ${eventCreator.address}`);
+    const fileContent = `export const eventFactoryAddress = "${eventCreator.address}";`;
+
+    //copy contract factory address into FS
+    fs.writeFile(
+        "../contracts/eventFactoryContractAddress.ts",
+        fileContent,
+        (err) => {
+            if (err) {
+                console.error(err);
+            }
+            // file written successfully
+        }
+    );
+    fs.writeFile(
+        "../frontend/src/eventFactoryContractAddress.ts",
+        fileContent,
+        (err) => {
+            if (err) {
+                console.error(err);
+            }
+            // file written successfully
+        }
+    );
+
+    //copy ABI into the front end folder
+    fs.copyFile(
+        "../contracts/artifacts/contracts/EventContractFactory.sol/EventContractFactory.json",
+        "../frontend/src/FactoryABI.json",
+        (err) => {
+            if (err) throw err;
+            console.log(err);
+        }
+    );
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+    console.error(error);
+    process.exitCode = 1;
 });
